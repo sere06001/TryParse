18b8cec72f781480e9b4c79ef9409e90c7726199
sere06001 <sere06001@utb.vaxjo.se>
2024-10-24 12:38:08 +0200

    Add project files.

---
+++ TryParse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!int.TryParse(Console.ReadLine(), out resultat))
+            {
+                Console.WriteLine("Skriv in en siffra.");
+            }
+            else
+            {
+                Console.WriteLine(resultat);
+            }
+        }
+        static bool TryParse(string s, out int result)
+        {
+            if (int.Parse(s) != null)
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            else
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
+}
